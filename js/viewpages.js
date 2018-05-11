5bd461e4679da9fbf3337a51f68146d61bf269a2
AleT <alet@risepic.com>
2018-05-11 09:33:44 +0200

    preview page update

--- js/viewpages.js
+++ js/viewpages.js
@@ -1,3 +1,15 @@
 $(document).ready(function () {
-        window.scrollTo(0,700);
-    });
+    window.scrollTo(0,700);
+});
+
+
+$(document).scroll(function(){
+ if($(window).scrollTop() > 150){
+   $('.qbox-prep').addClass('qbox-prep-before');
+   $('.qbox-prep').removeClass('qbox-prep-after');
+ }
+ else if($(window).scrollTop() < 150){
+   $('.qbox-prep').removeClass('qbox-prep-after');
+   $('.qbox-prep').addClass('qbox-prep-before');
+ }
+});
