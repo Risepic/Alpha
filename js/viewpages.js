7d888035bfed2ea876f12d73654e5fe207e17541
AleT <alet@risepic.com>
2018-05-15 18:17:02 +0200

    Update

--- js/viewpages.js
+++ js/viewpages.js
@@ -4,11 +4,11 @@ $(document).ready(function () {
 
 
 $(document).scroll(function(){
- if($(window).scrollTop() < 1600){
+ if($(window).scrollTop() < 1350){
    $('#qbox-prep').addClass('qbox-prep-before');
    $('#qbox-prep').removeClass('qbox-prep-after');
  }
- else if($(window).scrollTop() > 1600){
+ else if($(window).scrollTop() > 1350){
    $('#qbox-prep').addClass('qbox-prep-after');
    $('#qbox-prep').removeClass('qbox-prep-before');
  }
